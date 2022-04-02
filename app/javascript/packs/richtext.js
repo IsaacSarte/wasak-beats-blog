f7e920ca115b0b431c1b2a7889cac17acfd63f76
Isaac Sarte <aizakku09@gmail.com>
2022-04-02 20:17:16 +0800

    edit layout

--- app/javascript/packs/richtext.js
+++ app/javascript/packs/richtext.js
@@ -1,4 +1,7 @@
 document.addEventListener('turbolinks:load', () => {
+
+    console.log('Hello from richtext js');
+
     let hideEditorBtn = document.querySelector('.hide-editor')
     let addLyricsBtn = document.querySelector('.add-lyrics-btn')
     let richTextArea = document.querySelector('.rich-form')
@@ -6,11 +9,28 @@ document.addEventListener('turbolinks:load', () => {
     let openEditorBtn = document.querySelector('.open-editor')
     openEditorBtn.style.display = 'none'
 
+    let editorFooter = document.querySelector('.edit-footer')
+
     hideEditorBtn.addEventListener('click', () => {
         console.log('Hide Editor Button Clicked')
         addLyricsBtn.style.display = 'none'
         richTextArea.style.display = 'none'
         openEditorBtn.style.display = 'initial'
+
+        const phoneQuery = window.matchMedia("(max-width: 639px)")
+
+        if (phoneQuery.matches) {
+            editorFooter.style.cssText = `
+                position: relative;
+        `;
+        } else {
+            editorFooter.style.cssText = `
+            position: fixed;
+            bottom: 0;
+            max-width: 100%;
+            width: 100%;
+        `;
+        }
     })
 
     openEditorBtn.addEventListener('click', () => {
@@ -18,5 +38,6 @@ document.addEventListener('turbolinks:load', () => {
         openEditorBtn.style.display = 'none'
         addLyricsBtn.style.display = 'initial'
         richTextArea.style.display = 'block'
+        editorFooter.style.position = 'relative'
     })
 })
